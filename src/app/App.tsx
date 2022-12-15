821675a63b475aa50484f907343b380fed4f9b77
Miika H <mhalmetoja@gmail.com>
2022-12-15 03:14:21 +0200

    Adding some basic responsiveness

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,7 +10,7 @@ import * as React from "react";
 import { Helmet } from "react-helmet-async";
 import { Routes, Route, BrowserRouter, Link, NavLink } from "react-router-dom";
 import { useContext, Suspense } from "react";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 import "./App.css";
 //import { LoginStatusProvider } from '../utils/LoginStatusContext';
@@ -19,8 +19,11 @@ import "./App.css";
 export function App() {
   return (
     <>
-      <Helmet titleTemplate="%s" defaultTitle="Capitally.io">
-        <meta name="description" content="Capitally.io React application" />
+      <Helmet titleTemplate="%s" defaultTitle="Humavoid - Official">
+        <meta
+          name="description"
+          content="Humavoid - Official React application"
+        />
       </Helmet>
       <HomePage />
     </>
@@ -42,35 +45,65 @@ export function HomePage() {
     });
   };
 
+  const [background, setBackground] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 0) {
+        setBackground(true);
+      } else {
+        setBackground(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <main className="homepage-wrapper">
       <div className="section-wrapper">
-        <section className="home-section hp-section" ref={home}>
-          <header className="home-section-header">
-            <a
-              className="navigation-bar-burger-icon"
-              onClick={() => scrollToSection(store)}
-            >
-              <div className="fa fa-bars"></div>
-            </a>
-            <div className="humavoid-home-section-logo" />
-            <nav className="navigation-bar">
-              <ul className="nav-links-wrapper">
-                <li onClick={() => scrollToSection(home)}>HOME</li>
-                <li onClick={() => scrollToSection(bio)}>BIO</li>
-                <li onClick={() => scrollToSection(tour)}>TOUR</li>
-                <li onClick={() => scrollToSection(store)}>STORE</li>
-                <li onClick={() => scrollToSection(contact)}>CONTACT</li>
-                <li onClick={() => scrollToSection(contact)}>EPK</li>
-                <li onClick={() => scrollToSection(contact)}>SOCIAL</li>
-              </ul>
-            </nav>
-          </header>
-        </section>
-        <section className="bio-section hp-section" ref={bio}></section>
-        <section className="tour-section hp-section" ref={tour}></section>
-        <section className="store-section hp-section" ref={store}></section>
-        <section className="contact-section hp-section" ref={contact}></section>
+        <header
+          className={`${
+            background
+              ? "home-section-header-scrolled"
+              : "home-section-header-unscrolled"
+          }`}
+        >
+          <div
+            className="navigation-bar-burger-icon"
+            onClick={() => scrollToSection(store)}
+          >
+            <div className="fa fa-bars"></div>
+          </div>
+          <div className="humavoid-home-section-logo" />
+          <nav className="navigation-bar">
+            <ul className="nav-links-wrapper">
+              <li onClick={() => scrollToSection(home)}>HOME</li>
+              <li onClick={() => scrollToSection(bio)}>BIO</li>
+              <li onClick={() => scrollToSection(tour)}>TOUR</li>
+              <li onClick={() => scrollToSection(store)}>STORE</li>
+              <li onClick={() => scrollToSection(contact)}>CONTACT</li>
+              <li onClick={() => scrollToSection(contact)}>EPK</li>
+              <li onClick={() => scrollToSection(contact)}>SOCIAL</li>
+            </ul>
+          </nav>
+        </header>
+        <div className="home-section hp-section" ref={home}></div>
+        <div className="bio-section hp-section" ref={bio}>
+          <div className="smoke-1"></div>
+        </div>
+        <div className="tour-section hp-section" ref={tour}>
+          <div className="smoke-2"></div>
+        </div>
+        <div className="store-section hp-section" ref={store}>
+          <div className="smoke-1"></div>
+        </div>
+        <div className="contact-section hp-section" ref={contact}>
+          <div className="smoke-2"></div>
+        </div>
       </div>
     </main>
   );
