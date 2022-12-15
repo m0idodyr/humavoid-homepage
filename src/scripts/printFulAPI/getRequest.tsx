5f285ceeb82478545032e9321da5929f7ec7e5f0
Miika H <mhalmetoja@gmail.com>
2022-12-15 05:46:13 +0200

    Tweak

--- src/scripts/printFulAPI/getRequest.tsx
+++ src/scripts/printFulAPI/getRequest.tsx
@@ -1,32 +1,31 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
 async function getRequest() {
   try {
-    const response = await fetch('https://api.printful.com/store/products', {
+    const response = await fetch("https://api.printful.com/store/products", {
       headers: {
-        Authorization: 'Bearer JumRRORilzbrN4en0sEjJ5BJZvnARiz3Y5Si7lbi'
-      }
+        Authorization: "",
+      },
     });
 
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     }
 
-    const result = (await response.json())
+    const result = await response.json();
 
-    console.log('result is: ', JSON.stringify(result, null, 4));
-
+    console.log("result is: ", JSON.stringify(result, null, 4));
   } catch (error) {
     if (error instanceof Error) {
-      console.log('error message: ', error.message);
+      console.log("error message: ", error.message);
       return error.message;
     } else {
-      console.log('unexpected error: ', error);
-      return 'An unexpected error occurred';
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred";
     }
   }
-};
+}
 
-getRequest()
+getRequest();
 
-export default getRequest
+export default getRequest;
